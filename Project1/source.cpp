39c3460f5e97cd5fa5e9919dcd3d77dc0462cbd0
dontbecareless <tima_guryanov@vk.com>
2025-01-12 13:52:24 +0300

    Base abilities described
    BUT thera are critical error, and code didnt work (passive_abilites unknown size when we are describe status)

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -1,50 +1,27 @@
+//В конце разработки надо будет как то распихать всё это чудо по файлам и систематизировать, а также понаделать анимаций + звуков
 #include <SFML/Graphics.hpp>
 #include<vector>
 #include<iostream>
 #include<algorithm>
+//#include <cstdlib>//если рандом не захочет работать
+#include <ctime>
 using namespace sf;
 using namespace std;
 string globalState = "free";
 int select = -1;
 int damaged = -1;
 //string turn = "us"; Данный код будет нужен, когда враги не будут болванками для избиения
-struct active_ability {
-    int range;
-    string target;//can be:earth, friend, enemy, nontarget
-    vector<string> removedStatus;
-    vector<string> addedStatus;
-    int hpChange;
-};
-struct passive_ability {
-    string trigger;
-    vector<status> effects;
-    string target;
-    int area;
-    int hpChange;
-
-};//passives can triggered: when take damage, deal damage, move, start turn, end turn, get attacked, attack
-/*
-all stats are :
-movespeed
-dmg
-hpMax
-hp
-initiative
-*/
-struct status {
-    int msChange;
-    int dmgChange;
-    int hpmaxChange;
-    int intiativeChange;
-    bool applied = false;
-    int time;
-    vector<passive_ability> added_passives;
-};
+struct passive_ability;
+struct status;
+struct active_ability;
 struct unit {
     Texture txtt;
     int hp;
     int ms;
     int dmg;
+    int mindmg;
+    int maxdmg;
+    int deltadmg = 2;
     bool alive = true;
     int positionx;
     int positiony;
@@ -54,10 +31,21 @@ struct unit {
     int hpMax;
     bool hasUnappliedStatus = false;
     Sprite txt;
-    vector<active_ability> actives;
-    vector<passive_ability> passives_base;
-    vector<passive_ability> passives;
-    vector<status> statuses;
+    vector<pair<bool, int>> applied_status = {};
+    vector<active_ability> actives;//это все кажется можно сделать сетами и это будет оптимальнее
+    vector<status> statuses = {};
+    vector<passive_ability> passives_whenTurnStart_base = {};
+    vector<passive_ability> passives_whenTurnStart = {};
+    vector<passive_ability> passives_whenAttack_base = {};
+    vector<passive_ability> passives_whenAttack = {};
+
+    void applieAllStatus() {
+    }
+    void turnStartPassives() {
+    }
+    void attackPassives() {
+    }
+
     void takedmg(int tika, vector<vector<string>>& battlefieldState) {
         hp -= tika;
         if (hp > hpMax) {
@@ -73,6 +61,17 @@ struct unit {
         if (hp > hpMax && !barrier) {
             hp = hpMax;
         } else if (hp <= 0) {
+            alive = false;
+            battlefieldState[positiony][positionx] = "free";
+            hp = 1;
+        }
+    }
+    void takedmg(int midmg, int madmg, vector<vector<string>>& battlefieldState, bool barrier) {
+        hp -= (midmg + (rand() / ((RAND_MAX + 1u) / (madmg-midmg+1))));
+        if (hp > hpMax && !barrier) {
+            hp = hpMax;
+        }
+        else if (hp <= 0) {
             alive = false;
             battlefieldState[positiony][positionx] = "free";
             hp = 1;
@@ -97,11 +96,35 @@ struct unit {
         return initiative <= another.initiative;
     }
 };
+bool rtechnicallythetruth(unit& owner, vector<vector<string>>& battlefieldState) {
+    return true;
+}
+bool isEnemyNear(unit& owner, vector<vector<string>>& battlefieldState) {
+    int x = owner.positionx;
+    int y = owner.positiony;
+    string searching = "";
+    if (battlefieldState[y][x] == "enemy") {
+        searching = "friend";
+    }
+    else {
+        searching = "enemy";
+    }
+    for (int iy = max(0, y - 1); iy < max(6, y + 1); y++) {
+        for (int ix = max(0, x + 1); ix < max(11, x + 1); x++) {
+            if (battlefieldState[iy][ix] == searching) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 struct frienUnit : public unit {
     frienUnit(int hpn, int msn, int dmgn, int posx, int posy, int ini) {
         hp = hpn;
         ms = msn;
         dmg = dmgn;
+        mindmg = max(0,dmgn - deltadmg);
+        maxdmg = dmgn + deltadmg;
         positionx = posx;
         positiony = posy;
         initiative = ini;
@@ -213,9 +236,7 @@ struct frienUnit : public unit {
                 }
             }
             if (nowmovex == 0 && nowmovey == 0) {
-            //if (true) {
                 zhertva.takedmg(dmg, bfstate);
-                cout << "LOS" << endl;
                 damaged = -1;
                 select = -1;
                 globalState = "free";
@@ -237,10 +258,163 @@ struct enemyUnit : public unit {
     }
 };
 
+struct status {
+    int id;
+    int msChange = 0;
+    int dmgChange = 0;
+    int hpmaxChange = 0;
+    int hpChange = 0;
+    int initiativeChange = 0;
+    int time = 0;
+    vector<passive_ability> added_passives = {};
+    bool operator==(status& another) {
+        return id == another.id;
+    }
+    void firstApplie(unit& owner) {
+        for (int i = 0; i < added_passives.size(); i++) {
+            if (added_passives[i].trigger == "attack") {
+                if (find(owner.passives_whenAttack.begin(), owner.passives_whenAttack.end(), added_passives[i]) == owner.passives_whenAttack.end()) {
+                    owner.passives_whenAttack.push_back(added_passives[i]);
+                }
+            }
+            if (added_passives[i].trigger == "turn start") {
+                if (find(owner.passives_whenTurnStart.begin(), owner.passives_whenTurnStart.end(), added_passives[i]) == owner.passives_whenTurnStart.end()) {
+                    owner.passives_whenTurnStart.push_back(added_passives[i]);
+                }
+            }
+        }
+        owner.ms = max(0, owner.ms - msChange);
+        owner.dmg = max(0, owner.dmg - dmgChange);
+        owner.hpMax = max(0, owner.hpMax - hpmaxChange);
+        owner.hp = min(owner.hp, owner.hpMax);
+        owner.initiative -= initiativeChange;
+    }
+    void removestatus(unit& owner) {
+        int posOfStatus = find(owner.statuses.begin(), owner.statuses.end(), *this) - owner.statuses.begin();
+        owner.statuses.erase(owner.statuses.begin() + posOfStatus);
+        owner.applied_status.erase(owner.applied_status.begin() + posOfStatus);
+        for (int i = 0; i < added_passives.size(); i++) {
+            added_passives[i].removepassive(owner);
+        }
+        owner.hpMax -= hpmaxChange;
+        owner.ms -= msChange;
+        owner.dmg -= dmgChange;
+        owner.initiative -= initiativeChange;
+    }
+    void tick(unit& owner, vector<vector<string>>& battlefieldState) {
+        int timeNow=owner.applied_status[find(owner.statuses.begin(), owner.statuses.end(), *this) - owner.statuses.begin()].second--;
+        if (timeNow == 0) {
+            this->removestatus(owner);
+        }
+        owner.takedmg(hpChange,battlefieldState);
+    }
+};
+struct active_ability {
+    int id;
+    int range;
+    int area;
+    string target;//can be:earth, friend, enemy, nontarget
+    vector<status> removedStatus = {};
+    vector<status> addedStatus = {};
+    int hpChange;
+    bool operator==(active_ability& another) {
+        return id == another.id;
+    }
+};
+struct passive_ability {
+    int id;
+    int cd = 0;
+    bool (*condition)(unit&, vector<vector<string>>&) = rtechnicallythetruth;
+    string trigger;
+    vector<status> effects_enemy = {};
+    vector<status> effects_friends = {};
+    int area;
+    int hpChangeEnemy;
+    int hpChangeFriends;
+    bool operator==(passive_ability& another) {
+        return id == another.id;
+    }
+    void removepassive(unit& owner) {
+        if (trigger == "attack") {
+            owner.passives_whenAttack.erase(find(owner.passives_whenAttack.begin(), owner.passives_whenAttack.end(), this));
+        }
+        if (trigger == "turn start") {
+            owner.passives_whenTurnStart.erase(find(owner.passives_whenTurnStart.begin(), owner.passives_whenTurnStart.end(), this));
+        }
+    }
 
+};//passives can triggered: when take damage, deal damage, move, start turn, end turn, get attacked, attack
+/*
+all stats are :
+movespeed
+dmg
+hpMax
+hp
+initiative
+*/
 
 int main()
-{
+{   
+    //тут будут описаны способности и сопутствующие им статусы, всё это потом уедет в отдельный файл, с которым мне пока лень разбирваться
+    /*
+    МИНИМАЛЬНЫЙ НАБОР СПОСОБНОСТЕЙ
+    \/fireball (урон по области 3 на 3 на расстоянии до 4)
+    \/poison touch (отравление на 3 хода, в каждый ход получает по 1 урону)
+    RUNRUNRUN (ms+2 если рядом со врагом)
+    */
+    active_ability fireball;
+    fireball.range = 4;
+    fireball.addedStatus = {};
+    fireball.removedStatus = {};
+    fireball.target = "earth";
+    fireball.hpChange = 4;
+    fireball.area = 1;
+    fireball.id = 1;
+    status poisoned;
+    poisoned.id = 1;
+    poisoned.hpChange = 1;
+    poisoned.time = 3;
+    passive_ability poison_touch;
+    poison_touch.hpChangeFriends = 0;
+    poison_touch.hpChangeEnemy = 0;
+    poison_touch.area = 0;
+    poison_touch.cd = 0;
+    poison_touch.id = 1;
+    poison_touch.trigger = "attack";
+    poison_touch.effects_enemy.push_back(poisoned);
+    status running;
+    running.id = 2;
+    running.time = 1;
+    running.msChange = 2;
+    passive_ability runrunrun;
+    runrunrun.condition = isEnemyNear;
+    runrunrun.id = 2;
+    runrunrun.area = 0;
+    runrunrun.cd = 0;
+    runrunrun.effects_friends.push_back(running);
+    runrunrun.hpChangeEnemy = 0;
+    runrunrun.hpChangeFriends = 0;
+    runrunrun.trigger = "turn start";
+
+
+    
+
+
+
+
+
+
+
+
+
+    //w=11
+    //h=6
+
+    //способности и сопутствующие статусы закончились
+    srand(time(nullptr));//задаём сид для генерации случайных чисел (что это и почему это желательно сделать можно почитать на cppreferences)
+
+
+
     RenderWindow window(VideoMode(1500, 1000), L"Новый проект", Style::Fullscreen);
     window.setVerticalSyncEnabled(true);
     Clock rattle;
@@ -303,37 +477,6 @@ int main()
         battlefieldState[posyin - 1][posxin - 1] = "enemy";
         they.push_back(scytheofvyse);
     }
-    
-
-    /*int n = 1, hpin = 1, msin = 1, dmgin = 1, posxin = 1, posyin = 1, inin = 1;
-    // cout << "enter size of your army:" << endl;
-    // cin >> n;
-    vector<frienUnit> us;
-    for (int i = 0; i < n; i++) {
-        // cout << " enter hp, movespeed, damage, position x, position y and initiative of your "<<i+1<<"st unit"<<endl;
-        // cin >> hpin >> msin >> dmgin >> posxin >> posyin >> inin;
-        frienUnit scytheofvyse(hpin, msin, dmgin, posxin-1, posyin-1,inin);
-        battlefieldState[posyin-1][posxin-1] = "friend";
-        us.push_back(scytheofvyse);
-    }
-    n = 1, hpin = 2, msin = 2, dmgin = 2, posxin = 2, posyin = 2, inin = 2;
-    // cout << "enter size of enemy army:" << endl;
-    // cin >> n;
-    vector<enemyUnit> they;
-    for (int i = 0; i < n; i++) {
-        // cout << " enter hp, movespeed, damage, position x, position y and initiative of enemy " << i + 1 << "st unit" << endl;
-        // cin >> hpin >> msin >> dmgin >> posxin >> posyin >> inin;
-        enemyUnit scytheofvyse(hpin, msin, dmgin, posxin - 1, posyin - 1, inin);
-        battlefieldState[posyin - 1][posxin - 1] = "enemy";
-        they.push_back(scytheofvyse);
-    }
-
-
-
-    */
-
-
-
 
     //мб сделать таки массив в котором чары хранятся а не тупо поляну и отдельно массив чаров
 
@@ -383,7 +526,14 @@ int main()
                 select = i;
             }
         }*/
-
+        for (int i = 0; i < us.size(); i++) {
+            us[i].applieAllStatus();
+            us[i].turnStartPassives();
+        }
+        for (int i = 0; i < they.size(); i++) {
+            they[i].applieAllStatus();
+            they[i].turnStartPassives();
+        }
 
         for (int i = 0; i < they.size(); i++) {
             if (globalState == "free" &&they[i].alive==true && select != -1 && Mouse::isButtonPressed(Mouse::Left) && battlefield[they[i].positiony][they[i].positionx].getGlobalBounds().contains(Mouse::getPosition().x, Mouse::getPosition().y)) {
